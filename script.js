10cc9cb38aa4b475b46fa853d2d021c1ec4c1c2b
José David Portugues Aguilar <98204413+davidpaom@users.noreply.github.com>
2022-01-31 12:11:45 -0600

    Agregando pagina principal y secundaria

---
+++ script.js
@@ -0,0 +1,9 @@
+var ahorcado0 = document.getElementById("ahorcado0")
+var ahorcado1 = document.getElementById("ahorcado1")
+var ahorcado2 = document.getElementById("ahorcado2")
+var ahorcado3 = document.getElementById("ahorcado3")
+var ahorcado4 = document.getElementById("ahorcado4")
+var ahorcado5 = document.getElementById("ahorcado5")
+var ahorcado6 = document.getElementById("ahorcado6")
+
+ahorcado6.style.visibility = "hidden";
