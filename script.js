f3a166a5cf0a5ea02f2118d3c6f19fb4d3dddc9c
José David Portugues Aguilar <98204413+davidpaom@users.noreply.github.com>
2022-02-01 16:42:49 -0600

    Agregando validacion de escritura

--- script.js
+++ script.js
@@ -9,15 +9,20 @@ var ahorcado6 = document.getElementById("ahorcado6")
 //Declaración y llamado de las variables con los botones de jugar y agregar palabra
 var botonJugar = document.getElementById("btn_jugar")
 var botonAgregar = document.getElementById("btn_addword")
+var teclear = document.getElementById("entrada")
 //Declaración y llamado de las variables para la palabra y para las lineas.
 var palabra = document.getElementById("palabra")
 var lineas = document.getElementById("lineas")
+var oprimir = document.getElementById("oprimir")
 //Declaración de arreglo con las palabras
 var palabras = ["ALURA", "ORACLE", "CHALLENGE"]
+//Declaracion del arreglo de letras totales del alfabeto.
+var alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Ñ"]
 var randomWord = ""
 var randomWordArray = []
 var numeroDeLetras = 0;
 var guiones = ""
+var tecla = 0;
 //Esconder imagenes del ahorcado.
 ahorcado6.style.visibility = "hidden";
 ahorcado5.style.visibility = "hidden";
@@ -25,8 +30,6 @@ ahorcado4.style.visibility = "hidden";
 ahorcado3.style.visibility = "hidden";
 ahorcado2.style.visibility = "hidden";
 ahorcado1.style.visibility = "hidden";
-//
-
 //Función para sacar una palabara aleatoramente y contar el numero de letras en la palabra, generando el string de guiones
 function aleatorio () {
     let largo = palabras.length //Encontrar el tamaño del arreglo de palabras
@@ -42,9 +45,47 @@ function aleatorio () {
     guiones = guionesArray.join("") //Convertir el arreglo de guiones en String para imprimirlos debajo de la palabra.
 }
 
-aleatorio()
+//funcion para tomar el valor de la tecla presionada, checar si es una letra del alfabeto y si no mostrar un mensaje. También imprime en el html
+// la letra para que el usuario la vea.
+function imprimir (e){
+    //If general para saber si el codigo de la tecla tecleada corresponder a alguna letra del alfabeto o a la ñ en Mayuscula o Minuscula.
+    if((e.which >= 65 && e.which<=90)||(e.which >= 97 && e.which<=122)||e.which==209||e.which==241){
+        //If para identificar si es letra del alfabeto maysucula e imprimir el valor del arreglo Alfabeto antes creado. Corresponden a las letras.
+        if(e.which >= 65 && e.which<=90){
+            tecla = e.which-65
+            oprimir.textContent = alfabeto[tecla]
+        }
+        //If If para identificar si es letra del alfabeto minuscula e imprimir el valor del arreglo Alfabeto antes creado. Corresponden a las letras.
+        if(e.which >= 97 && e.which<=122){
+            tecla = e.which-97
+            oprimir.textContent = alfabeto[tecla]
+        }
+        //If para identificar si es la letra eñe en mayuscula o minuscula e imprimirla.
+        if(e.which==209||e.which==241){
+            tecla = 26
+            oprimir.textContent = alfabeto[tecla]
+        }
+    } else {
+        alert("RECUERDA QUE SOLO PUEDES TECLEAR LETRAS")
+    }
+}
+
+function focus(){
+    entrada.focus()
+}
+
+aleatorio() //Llamada a la función para sortear las palabras, se hace cada vez que empezamos el juego.
+lineas.textContent = guiones //Impresión de los guiones debajo de las palabras.
+
+entrada.focus() //Enfocarse en el input cada que empiece la página
+
+entrada.addEventListener('keypress',imprimir) //Llamada a la función cada vez que se oprima una tecla. Se checará si la letra es tecla
+//y si está dentro del alfabeto, de otra forma se mostrará una alerta.
+
+setInterval(focus,1) //Llamada a la función para enfocarse en nuestro input que recibirá las letras.
+
 palabra.textContent = randomWord
-lineas.textContent = guiones
+
 
 
 
